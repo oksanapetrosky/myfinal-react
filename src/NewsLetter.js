a8ca4dd5159a68dc5ce1bf6d61f45b88c2c15ea1
oksanapetrosky <165575688+oksanapetrosky@users.noreply.github.com>
2024-10-18 09:58:46 -0400

    Update NewsLetter.js

---
+++ src/NewsLetter.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const Newsletter = () => {
+  const [email, setEmail] = useState('');
+  const [status, setStatus] = useState(''); 
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email) {
+      setStatus('Please enter a valid email address');
+      return;
+    }
+    setStatus('');
+    setEmail(''); 
+  };
+
+  return (
+    <div className='background'>
+       <h5>Subscribe to our Newsletter</h5>
+     <div className="newsletter">
+     
+      <form id="form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+          required/>
+        <button type="submit">Subscribe</button>
+    
+      </form>
+      {status && <p>{status}</p>} 
+    </div>
+    </div>
+   
+  );
+};
+
+export default Newsletter;
